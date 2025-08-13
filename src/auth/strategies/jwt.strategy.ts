8fb7790349e44cdd5d962ac8525337def957ae5a
caadpo <caadpo@gmail.com>
2025-08-13 14:02:15 -0300

    correção na aplicaçãoo

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -3,16 +3,16 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { LoginPayload } from '../dtos/loginPayload.dto';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      // src/auth/strategies/jwt.strategy.ts
       jwtFromRequest: ExtractJwt.fromExtractors([
-        (req) => req?.cookies?.accessToken,
+        (req: Request) => req?.cookies?.accessToken, // Cookies
+        ExtractJwt.fromAuthHeaderAsBearerToken(),    // Header Authorization: Bearer
       ]),
-
       secretOrKey: process.env.JWT_SECRET,
     });
   }
